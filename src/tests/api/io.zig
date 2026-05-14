972e8d805d86b677f680e60e6be585a86b440be3
Daniel Philipov <batkodanny@gmail.com>
2026-05-14 17:30:13 -0500

    new tests

--- src/tests/api/io.zig
+++ src/tests/api/io.zig
@@ -1,22 +1,49 @@
 const util = @import("../util.zig");
 const Io = @import("../../api/io.zig");
 const NullIO = Io.NullIO;
-const MemIO = Io.MemIO;
-const SeekIO = Io.SeekIO;
 
 pub fn run() util.test_results {
     return util.run_tests("IO", &.{
-        // nullio
-        .{.name = "NullIO Read", .func = nullio_read},
-        .{.name = "NullIO Write", .func = nullio_write},
-        // memio
-        // seekio
+        .{ .name = "NullIO Read", .func = nullio_read },
+        .{ .name = "NullIO Write", .func = nullio_write },
+        .{ .name = "Unsupported Defaults", .func = unsupported_defaults },
+        .{ .name = "Fill Reads Multiple Chunks", .func = fill_reads_multiple_chunks },
+        .{ .name = "Addref Close Once", .func = addref_close_once },
     });
 }
 
+const MinimalIO = struct {
+    io: Io = .from(MinimalIO),
+};
+
+const ChunkedIO = struct {
+    io: Io = .from(ChunkedIO),
+    read_chunk: usize = 2,
+    read_calls: usize = 0,
+    next_byte: u8 = 0,
+    close_calls: usize = 0,
+
+    pub fn read(ioptr: *Io, buf: []u8) Io.Error!usize {
+        const self: *ChunkedIO = @fieldParentPtr("io", ioptr);
+        const n = @min(buf.len, self.read_chunk);
+
+        for (buf[0..n]) |*b| {
+            b.* = self.next_byte;
+            self.next_byte +%= 1;
+        }
+
+        self.read_calls += 1;
+        return n;
+    }
+
+    pub fn close(ioptr: *Io) void {
+        const self: *ChunkedIO = @fieldParentPtr("io", ioptr);
+        self.close_calls += 1;
+    }
+};
 
 fn nullio_read() anyerror!void {
-    var buf = [_]u8{0xFF, 1, 2};
+    var buf = [_]u8{ 0xFF, 1, 2 };
     var self: NullIO = .{};
     const num = try self.io.read(&buf);
     try util.expect(num == buf.len);
@@ -26,8 +53,7 @@ fn nullio_read() anyerror!void {
 }
 
 fn nullio_write() anyerror!void {
-    // FIXME: I think I need to write my own allocator
-    const exbuf: [5]u8 = [_]u8{1, 2, 3, 4, 5};
+    const exbuf: [5]u8 = [_]u8{ 1, 2, 3, 4, 5 };
     var self: NullIO = .{};
     const written = try self.io.write(&exbuf);
 
@@ -35,6 +61,39 @@ fn nullio_write() anyerror!void {
     try util.expect(exbuf[2] == 3);
 }
 
-// TODO: memio readat
-// TODO: memio writeat
-// TODO: memio
+fn unsupported_defaults() anyerror!void {
+    var buf = [_]u8{ 1, 2, 3 };
+    var self: MinimalIO = .{};
+
+    try util.expect(try self.io.cntl(Io.IOCTL_GETBLKSZ, null) == 1);
+    try util.expectError(Io.Error.Unsupported, self.io.read(&buf));
+    try util.expectError(Io.Error.Unsupported, self.io.readat(&buf, 0));
+    try util.expectError(Io.Error.Unsupported, self.io.write(&buf));
+    try util.expectError(Io.Error.Unsupported, self.io.writeat(&buf, 0));
+}
+
+fn fill_reads_multiple_chunks() anyerror!void {
+    var buf = [_]u8{0} ** 5;
+    var self: ChunkedIO = .{};
+
+    try self.io.fill(&buf);
+
+    try util.expect(self.read_calls == 3);
+    for (buf, 0..) |b, i|
+        try util.expect(b == i);
+}
+
+fn addref_close_once() anyerror!void {
+    var self: ChunkedIO = .{};
+
+    _ = Io.addref(&self.io);
+    _ = Io.addref(&self.io);
+
+    self.io.close();
+    try util.expect(self.close_calls == 0);
+    try util.expect(self.io.refcnt == 1);
+
+    self.io.close();
+    try util.expect(self.close_calls == 1);
+    try util.expect(self.io.refcnt == 0);
+}
--- src/tests/mem/page.zig
+++ src/tests/mem/page.zig
@@ -1,12 +1,39 @@
 const util = @import("../util.zig");
 const page = @import("../../mem/page.zig");
 
-
 pub fn run() util.test_results {
     return util.run_tests("PAGE", &.{
-        .{.name = "Allocate all pages", .func = allocateAll},
-        .{.name = "Page coalescing", .func = coalescing}
+        .{ .name = "Page rounding", .func = pageRounding },
+        .{ .name = "Allocated pages are zeroed", .func = allocatedPagesAreZeroed },
+        .{ .name = "Allocate all pages", .func = allocateAll },
+        .{ .name = "Page coalescing", .func = coalescing },
+        .{ .name = "Middle free coalescing", .func = middleFreeCoalescing },
     });
+}
+
+fn pageRounding() anyerror!void {
+    try util.expect(page.round_down(0) == 0);
+    try util.expect(page.round_up(0) == 0);
+    try util.expect(page.round_down(page.SIZE - 1) == 0);
+    try util.expect(page.round_up(page.SIZE - 1) == page.SIZE);
+    try util.expect(page.round_down(page.SIZE + 1) == page.SIZE);
+    try util.expect(page.round_up(page.SIZE + 1) == 2 * page.SIZE);
+}
+
+fn allocatedPagesAreZeroed() anyerror!void {
+    const pp = page.phys_alloc(2);
+
+    pp[0] = 0xAA;
+    pp[page.SIZE] = 0xBB;
+    pp[2 * page.SIZE - 1] = 0xCC;
+    page.phys_free(pp);
+
+    const pp2 = page.phys_alloc(2);
+    defer page.phys_free(pp2);
+
+    try util.expect(pp2[0] == 0);
+    try util.expect(pp2[page.SIZE] == 0);
+    try util.expect(pp2[2 * page.SIZE - 1] == 0);
 }
 
 fn allocateAll() anyerror!void {
@@ -20,6 +47,30 @@ fn allocateAll() anyerror!void {
 
     page.phys_free(pp);
 
+    try util.expect(page.free_page_cnt() == orig);
+    try util.expect(page.free_chunk_cnt() == 1);
+}
+
+fn middleFreeCoalescing() anyerror!void {
+    const orig = page.free_page_cnt();
+    try util.expect(page.free_chunk_cnt() == 1);
+
+    const pp0 = page.phys_alloc(1);
+    const pp1 = page.phys_alloc(1);
+    const pp2 = page.phys_alloc(1);
+
+    try util.expect(page.free_page_cnt() + 3 == orig);
+    try util.expect(page.free_chunk_cnt() == 1);
+
+    page.phys_free(pp1);
+    try util.expect(page.free_page_cnt() + 2 == orig);
+    try util.expect(page.free_chunk_cnt() == 2);
+
+    page.phys_free(pp0);
+    try util.expect(page.free_page_cnt() + 1 == orig);
+    try util.expect(page.free_chunk_cnt() == 2);
+
+    page.phys_free(pp2);
     try util.expect(page.free_page_cnt() == orig);
     try util.expect(page.free_chunk_cnt() == 1);
 }
